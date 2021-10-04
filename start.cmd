2e431f5b92033d3bf2b3f1989d28f2be515b1861
HixXNetwork <skywarsmcpe2@gmail.com>
2021-10-04 14:11:06 -0700

    bruh

--- start.cmd
+++ start.cmd
@@ -1,3 +1,16 @@
 @echo off
 
-bin\node.exe start.js
+if exist node_modules (
+    if exist bin\node.exe (
+        bin\node.exe start.js
+    ) else (
+        echo Cannot find node.exe file please reinstall BlueBird
+    )
+) else (
+    bin\npm.cmd install
+    if exist bin\node.exe (
+        bin\node.exe start.js
+    ) else (
+        echo Cannot find node.exe file please reinstall BlueBird
+    )
+)
