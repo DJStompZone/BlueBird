fed9afc6959b7d5bfe34e55c3d6d5838cb99e7eb
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-11-15 09:35:35 +0300

    Create DataPacket.js

---
+++ src/bluebirdteam/network/mcpe/protocol/DataPacket.js
@@ -0,0 +1,88 @@
+const NetworkBinaryStream = require("../NetworkBinaryStream");
+const Server = require("../../../Server");
+
+class DataPacket extends NetworkBinaryStream{
+
+    static get NETWORK_ID(){ return 0; }
+
+    static get PID_MASK (){ return 0x3ff; }//10 bits
+
+    static get SUBCLIENT_ID_MASK(){ return 0x03; }
+    static get SENDER_SUBCLIENT_ID_SHIFT(){ return 10; }
+    static get RECIPIENT_SUBCLIENT_ID_SHIFT(){ return 12; }
+
+    initVars(){
+        this.isEncoded = false;
+        this.__encapsulatedPacket = null;
+
+        this.senderSubId = 0;
+        this.recipientSubId = 0;
+
+        this.server = new Server();
+    }
+
+    constructor() {
+        super();
+        this.initVars();
+    }
+
+    pid(){
+        return self.NETWORK_ID;
+    }
+
+    getName(){
+        return self.__filename.replace(".js", "");
+    }
+
+    canBeBatched(){
+        return true;
+    }
+
+    canBeSentBeforeLogin(){
+        return false;
+    }
+
+    mayHaveUnreadBytes(){
+        return false;
+    }
+
+    decode(){
+        this.offset = 0;
+        this.decodeHeader();
+        this.decodePayload();
+    }
+
+    decodeHeader(){
+        let header = this.readUnsignedVarInt();
+        let pid = header & self.PID_MASK;
+        if(pid !== self.NETWORK_ID){
+            console.log(`Expected ${self.NETWORK_ID} for packet ID, got ${pid}`);
+            this.server.shutdown();
+        }
+        this.senderSubId = (header >> self.SENDER_SUBCLIENT_ID_SHIFT) & self.SUBCLIENT_ID_MASK;
+        this.recipientSubId = (header >> self.RECIPIENT_SUBCLIENT_ID_SHIFT) & self.SUBCLIENT_ID_MASK;
+    }
+
+    decodePayload(){}
+
+    encodeHeader(){
+        this.writeUnsignedVarInt(
+            self.NETWORK_ID |
+            (this.senderSubId << self.SENDER_SUBCLIENT_ID_SHIFT) |
+            (this.recipientSubId << self.RECIPIENT_SUBCLIENT_ID_SHIFT)
+        );
+    }
+
+    encodePayload(){}
+
+    handle(session){}
+
+    clean(){
+        this.buffer = "";
+        this.isEncoded = false;
+        this.offset = 0;
+        return this;
+    }
+}
+
+module.exports = DataPacket;
