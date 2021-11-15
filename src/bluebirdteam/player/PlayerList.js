9cebd3529d3a373ef3347ab1868a462ef72cd149
DRAGKILLS <70339357+DRAGKILLS@users.noreply.github.com>
2021-11-15 10:40:50 +0300

    Create PlayerList.js

---
+++ src/bluebirdteam/player/PlayerList.js
@@ -0,0 +1,26 @@
+const Player = require("./Player");
+
+class PlayerList extends Map {
+    addPlayer(id, player){
+        CheckTypes([Player, player]);
+        this.set(id, player);
+    }
+
+    getPlayer(id){
+        return this.has(id) ? this.get(id) : null;
+    }
+
+    hasPlayer(player){
+        return Array.from(this.values()).indexOf(player) !== -1;
+    }
+
+    getPlayerIdentifier(player){
+        return Array.from(this.keys())[Array.from(this.values()).indexOf(player)];
+    }
+
+    removePlayer(id){
+        return this.delete(id);
+    }
+}
+
+module.exports = PlayerList;
